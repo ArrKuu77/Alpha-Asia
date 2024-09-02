769168dd82eb3671710954f6584dbff66c47944d
ArrKuu77 <aungkungsan@gmail.com>
2024-09-01 22:59:10 -1100

    Pdf-Function-Update

--- src/pages/ReadyDoctorList.jsx
+++ src/pages/ReadyDoctorList.jsx
@@ -17,30 +17,20 @@ const ReadyDoctorList = ({
   CurrentDate,
   Township,
 }) => {
-  console.log(DoctorList);
   const pdfRef = useRef();
-  // const downloadPDFpage = (D, w) => {
-  //   console.log(D, w, CurrentDate);
-  //   const input = pdfRef.current;
-  //   html2canvas(input).then((canvas) => {
-  //     var imgData = canvas.toDataURL("image/png");
-  //     const doc = new jsPDF("landscape", "px", "a4", true, true);
-  //     var imgHeight = (canvas.height * 208) / canvas.width;
-  //     doc.addImage(imgData, 0, 0, 208, imgHeight);
-  //     doc.save("result.pdf");
-  //   });
-  // };
 
   const [DoctorCallList, setDoctorCallList] = useState(
     localStorage.getItem("DoctorCallList") == null
       ? []
       : JSON.parse(localStorage.getItem("DoctorCallList"))
   );
+
   const [doctorArray, setdoctorArray] = useState(
     DoctorCallList.map((doctor) => doctor.DoctorName.toLowerCase())
   );
-  console.log(DoctorCallList);
-  console.log(doctorArray);
+
+  const [loadingProgress, setLoadingProgress] = useState(0); // State to track loading progress
+
   const downloadPDF = (D, w) => {
     const swalWithBootstrapButtons = Swal.mixin({
       customClass: {
@@ -49,226 +39,174 @@ const ReadyDoctorList = ({
       },
       buttonsStyling: true,
     });
+
     swalWithBootstrapButtons
       .fire({
-        title: "Are you sure Download PDF File?",
+        title: "Are you sure you want to download the PDF?",
         text: "You won't be able to revert this!",
         icon: "info",
         showCancelButton: true,
-        confirmButtonText: "Yes, Download it!",
+        confirmButtonText: "Yes, download it!",
         cancelButtonText: "No, cancel!",
         reverseButtons: true,
       })
       .then((result) => {
         if (result.isConfirmed) {
           if (D) {
-            const newDoctor = DoctorList.map((doctor) => {
-              return {
-                DoctorName: doctor.DoctorName,
-                DoctorFrequency: 1,
-                DoctorState: "Empty",
-              };
-            });
-            DoctorCallList.map((doctorCall) => {
-              newDoctor.map((newdoctor) => {
+            const newDoctor = DoctorList.map((doctor) => ({
+              DoctorName: doctor.DoctorName,
+              DoctorFrequency: 1,
+              DoctorState: "Empty",
+            }));
+
+            DoctorCallList.forEach((doctorCall) => {
+              newDoctor.forEach((newdoctor) => {
                 if (
-                  doctorCall.DoctorName.toLowerCase() ==
+                  doctorCall.DoctorName.toLowerCase() ===
                   newdoctor.DoctorName.toLowerCase()
                 ) {
                   doctorCall.DoctorFrequency += 1;
                 }
-                return doctorCall;
               });
             });
 
-            localStorage.setItem(
-              "DoctorCallList",
-              JSON.stringify([...DoctorCallList])
+            const trueDoctor = newDoctor.filter(
+              (ndoctor) =>
+                !doctorArray.includes(ndoctor.DoctorName.toLowerCase())
             );
 
-            const trueDoctor = newDoctor.filter((ndoctor) => {
-              console.log(doctorArray.includes(ndoctor.DoctorName));
-              if (doctorArray.includes(ndoctor.DoctorName.toLowerCase())) {
-                console.log(ndoctor);
-              } else {
-                console.log(ndoctor);
-
-                return ndoctor;
-              }
-            });
-            console.log(trueDoctor);
             setDoctorCallList([...DoctorCallList, ...trueDoctor]);
             setdoctorArray([
               ...doctorArray,
               ...trueDoctor.map((d) => d.DoctorName.toLowerCase()),
             ]);
+
             localStorage.setItem(
               "DoctorCallList",
               JSON.stringify([...trueDoctor, ...DoctorCallList])
             );
           }
 
-          console.log(D, w, CurrentDate);
-          console.log(pdfRef.current.offsetWidth, pdfRef.current.offsetHeight);
-          // console.log(window.innerWidth + 300);
-
-          var opt = {
+          const opt = {
             margin: 0.5,
             filename: `${D ? CurrentDate + D : CurrentDate + w}`,
             image: { type: "jpeg", quality: 0.98 },
             html2canvas: {
-              // windowWidth:2000,
-
-              width: pdfRef.current.offsetWidth,
-              scale: 1.5,
+              scale: 2, // Increase scale to improve rendering
+              onclone: (clonedDoc) => {
+                setLoadingProgress(0); // Reset progress when cloning starts
+              },
+              onprogress: (progress) => {
+                setLoadingProgress(Math.floor(progress * 100)); // Update progress state
+              },
             },
-            jsPDF: { unit: "mm", format: "a4", orientation: "landscape" },
+            jsPDF: { unit: "pt", format: "a4", orientation: "landscape" },
           };
-          console.log(opt, pdfRef.current);
-          html2pdf().set(opt).from(pdfRef.current).save();
+
+          const isSafari = /^((?!chrome|android).)*safari/i.test(
+            navigator.userAgent
+          );
+          if (isSafari) {
+            opt.html2canvas.scale = 1; // Adjust scale specifically for Safari
+          }
+
+          setTimeout(() => {
+            html2pdf().set(opt).from(pdfRef.current).save();
+          }, 100); // Adding a slight delay for rendering in Safari
 
           swalWithBootstrapButtons.fire({
-            title: "Download!",
-            text: "Your file has been Download.",
+            title: "Downloaded!",
+            text: "Your file has been downloaded.",
             icon: "success",
           });
-        } else if (
-          /* Read more about handling dismissals below */
-          result.dismiss === Swal.DismissReason.cancel
-        ) {
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
           swalWithBootstrapButtons.fire({
-            title: "Cancle",
-            text: "The file was not downloaded ",
+            title: "Cancelled",
+            text: "The file was not downloaded.",
             icon: "error",
           });
         }
       });
-
-    // const input = pdfRef.current;
-    // html2canvas(input).then((canvas) => {
-    //   const imgData = canvas.toDataURL("image/png");
-    // const pdf = new jsPDF("landscape", "px", "a4", true, true);
-    // const pdfWidth = pdf.internal.pageSize.getWidth();
-    //   const pdfHeight = pdf.internal.pageSize.getHeight();
-    //   const imageWidth = canvas.width;
-    //   const imageHeight = canvas.height;
-    //   const ratio = Math.min(pdfWidth / imageWidth, pdfHeight / imageHeight);
-    //   const imgX = (pdfWidth - imageWidth * ratio) / 2;
-    //   const imgY = 0;
-    //   console.log(imageWidth, imageHeight);
-    //   pdf.addImage(
-    //     imgData,
-    //     "PNG",
-    //     imgX,
-    //     imgY,
-    //     imageWidth * ratio,
-    //     imageHeight * ratio
-    //   );
-    //   console.log(saveDone);
-    //   D !== null && pdf.save(`${CurrentDate + " " + D}`);
-    //   w !== null && pdf.save(`${CurrentDate + " " + w}`);
-    //   console.log(saveDone);
-    // });
   };
+
   return (
     <>
       {DoctorList.length > 0 ? (
         <Template>
-          <div className="  maxContent bg-slate-500 pt-2 p-1 ">
-            <div className=" p-3 w-full  maxContent" ref={pdfRef}>
-              <div className="flex justify-between items-center ">
-                <div className=" flex items-center">
-                  <label htmlFor="" className=" font-medium text-xl block  ">
-                    MR Name -
-                  </label>
-                  <span className="block text-xl  text-black 	">
-                    {localStorage.getItem("MrName") == null
-                      ? "Your Name"
-                      : localStorage.getItem("MrName")}
+          <div className="maxContent bg-slate-500 pt-2 p-1">
+            <div className="p-3 w-full maxContent" ref={pdfRef}>
+              <div className="flex justify-between items-center">
+                <div className="flex items-center">
+                  <label className="font-medium text-xl">MR Name -</label>
+                  <span className="text-xl text-black">
+                    {localStorage.getItem("MrName") || "Your Name"}
                   </span>
                 </div>
-                <div className=" flex items-center">
-                  <label htmlFor="" className=" font-medium text-xl block  ">
-                    Date -
-                  </label>
-                  <span className="block text-xl  text-black 	">
-                    {CurrentDate}
-                  </span>
+                <div className="flex items-center">
+                  <label className="font-medium text-xl">Date -</label>
+                  <span className="text-xl text-black">{CurrentDate}</span>
                 </div>
-                <div className=" flex items-center">
-                  <label htmlFor="" className=" font-medium text-xl block  ">
-                    Township -
-                  </label>
-                  <span className="block text-xl  text-black 	">{Township}</span>
+                <div className="flex items-center">
+                  <label className="font-medium text-xl">Township -</label>
+                  <span className="text-xl text-black">{Township}</span>
                 </div>
               </div>
-              <table className=" mx-auto mb-10 mt-4 table-auto text-center  w-full border-collapse border border-slate-900 ">
+              <table className="mx-auto mb-10 mt-4 table-auto text-center w-full border-collapse border border-slate-900">
                 <thead>
                   <tr className="border border-slate-900 p-2">
-                    <th className="border border-slate-900 p-2">No</th>
-                    <th className="border border-slate-900 p-2">
-                      Customer Name
-                    </th>
-                    <th className="border border-slate-900 p-2">
-                      Call Purpose
-                    </th>
-                    <th className="border border-slate-900 p-2">
-                      Product Name
-                    </th>
-                    <th className="border border-slate-900 p-2">
-                      Customer's Feedback
-                    </th>
-                    <th className="border border-slate-900 p-2">
-                      Action plan form next call
-                    </th>
-
-                    {/* <th className=" text-center">Edit</th> */}
+                    <th className="border p-2">No</th>
+                    <th className="border p-2">Customer Name</th>
+                    <th className="border p-2">Call Purpose</th>
+                    <th className="border p-2">Product Name</th>
+                    <th className="border p-2">Customer's Feedback</th>
+                    <th className="border p-2">Action plan for next call</th>
                   </tr>
                 </thead>
                 <tbody>
-                  {DoctorList.map((list, index) => {
-                    return (
-                      <tr key={index} className="border border-slate-900">
-                        <td className="border p-3 border-slate-900 text-center">
-                          {" "}
-                          {index + 1}.{" "}
-                        </td>
-                        <td className="border p-3 border-slate-900 text-center">
-                          {" "}
-                          {list.DoctorName}{" "}
-                        </td>
-                        <td className="border p-3 border-slate-900">
-                          {list.CallPurpose}
-                        </td>
-                        <td className="border p-3 border-slate-900">
-                          {list.ShortName.length <= 0
-                            ? "-"
-                            : list.ShortName.map((name) => name + ",")}
-                        </td>
-                        <td className="border p-3 border-slate-900">
-                          {list.CustomerFeedback}
-                        </td>
-                        <td className="border p-3 border-slate-900">
-                          {list.NextPlan}
-                        </td>
-                      </tr>
-                    );
-                  })}
+                  {DoctorList.map((list, index) => (
+                    <tr key={index} className="border border-slate-900">
+                      <td className="border p-3 text-center">{index + 1}</td>
+                      <td className="border p-3 text-center">
+                        {list.DoctorName}
+                      </td>
+                      <td className="border p-3">{list.CallPurpose}</td>
+                      <td className="border p-3">
+                        {list.ShortName.length <= 0
+                          ? "-"
+                          : list.ShortName.join(", ")}
+                      </td>
+                      <td className="border p-3">{list.CustomerFeedback}</td>
+                      <td className="border p-3">{list.NextPlan}</td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
 
               <SalesForm />
             </div>
-            <div className=" ">
+
+            {loadingProgress > 0 && loadingProgress < 100 && (
+              <div className="my-4">
+                <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                  <div
+                    className="bg-blue-600 h-2.5 rounded-full"
+                    style={{ width: `${loadingProgress}%` }}
+                  ></div>
+                </div>
+                <p className="text-center mt-2">{loadingProgress}%</p>
+              </div>
+            )}
+
+            <div className="mt-4">
               <button
-                className="bg-indigo-500 p-2 rounded-md border-gray-900 border-2 mx-auto mt-2 text-start block"
-                onClick={downloadPDF.bind(null, "  DailyReport", null)}
+                className="bg-indigo-500 p-2 rounded-md border-gray-900 border-2 mx-auto mt-2 block"
+                onClick={() => downloadPDF("  DailyReport", null)}
               >
                 Download PDF For Daily
               </button>
               <button
-                className="bg-indigo-500 p-2 rounded-md border-gray-900 border-2 mx-auto mt-2 text-start block"
-                onClick={downloadPDF.bind(null, null, "  WeeklyReport")}
+                className="bg-indigo-500 p-2 rounded-md border-gray-900 border-2 mx-auto mt-2 block"
+                onClick={() => downloadPDF(null, "  WeeklyReport")}
               >
                 Download PDF For Weekly
               </button>
@@ -276,8 +214,8 @@ const ReadyDoctorList = ({
           </div>
         </Template>
       ) : (
-        <div className=" border border-spacing-10 m-4">
-          <h1 className=" ">There are no Lists ! </h1>
+        <div className="border border-spacing-10 m-4">
+          <h1>There are no lists!</h1>
         </div>
       )}
     </>
